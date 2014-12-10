bd6372e496114e7e25d478980d0d783278a69d9d
Sidney Harrell <sidney.harrell@gmail.com>
2014-12-09 23:08:36 -0500

    Update CustomEE4FunctionsPlugin.php
    updated to work post-gwrw

--- CustomEE4FunctionsPlugin.php
+++ CustomEE4FunctionsPlugin.php
@@ -10,7 +10,8 @@
  */
 define( 'CUSTOM_FUNCTIONS_MAIN_FILE', __FILE__ );
 define( 'CF_PLUGIN_DIR_PATH', plugin_dir_path( CUSTOM_FUNCTIONS_MAIN_FILE ));
-add_action('AHEE_EE_Paypal_Standard_form_build', 'add_my_field');
-function add_my_field($ee_paypal_standard) {
-    $ee_paypal_standard->addField('handling', '4.2');    
+add_filter('FHEE_EEG_Paypal_Standard_set_redirection_info_arguments', 'add_my_field');
+function add_my_field($redirect_args) {
+    $redirect_args['handling'] = '4.2');
+    return $redirect_args
 }
