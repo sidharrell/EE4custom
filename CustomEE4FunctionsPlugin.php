2410fe9cc1673dc7a82819887365cd2807d61a63
Sidney Harrell <sidney.harrell@gmail.com>
2014-12-09 23:29:09 -0500

    Update CustomEE4FunctionsPlugin.php

--- CustomEE4FunctionsPlugin.php
+++ CustomEE4FunctionsPlugin.php
@@ -8,10 +8,8 @@
  * Author URI: http://sidneyharrell.com
  * License: GPL2
  */
-define( 'CUSTOM_FUNCTIONS_MAIN_FILE', __FILE__ );
-define( 'CF_PLUGIN_DIR_PATH', plugin_dir_path( CUSTOM_FUNCTIONS_MAIN_FILE ));
 add_filter('FHEE_EEG_Paypal_Standard_set_redirection_info_arguments', 'add_my_field');
 function add_my_field($redirect_args) {
-    $redirect_args['handling'] = '4.2');
-    return $redirect_args
+    $redirect_args['handling'] = '4.2';
+    return $redirect_args;
 }
